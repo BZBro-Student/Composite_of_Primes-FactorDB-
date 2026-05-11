bc1c1d9674eea4d33cb85fbe0cb15f1b4a3283e2
Bzbro-Student <brodenbenson@pm.me>
2026-05-10 20:11:57 -0600

    change testing numbers

--- SemiPrimeFactorDB/Master/MasterMain.py
+++ SemiPrimeFactorDB/Master/MasterMain.py
@@ -1,7 +1,7 @@
 from Master import Master
 def main():
-    digit = 99
-    batchSize = 1000
+    digit = 20
+    batchSize = 5
     hostNames = ["bzbro@bropiSL1.local","bzbro@bropiSL2.local","bzbro@bropiSL2.local"]
     master = Master(hostNames, digit, batchSize)
     out = master.SendBatch(hostNames[0])
